9f46f261b08b736b044e8b8bfb68cd2a269bbfef
schwar12 <schwar12@purdue.edu>
2015-07-10 17:38:47 -0500

    Readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# ctci_v6
+Cracking the Coding Interview problems
 
-Cracking the Coding Interview v6 Exercises
+ch01 - 8 / 9
+ch02 - 6 / 8
+ch03 - 0 / 6
+ch04 - 0 / 12
+ch05 - 1 / 8
+ch06 - 0 / 10
+ch07 - 0 / 9
+ch08 - 4 / 14
+ch09 - 0 / 8
+ch10 - 0 / 11
+ch11 - 2 / 6
+ch12 - 0 / 11
+ch13 - 0 / 8
+ch14 - 0 / 7
+ch15 - 0 / 7
+ch16 - 1 / 26
+ch17 - 0 / 26
+
+Total
+22 / 189 problems
+
--- count.sh
+++ count.sh
@@ -1,3 +1,14 @@
-#echo */*/{*.java,*.c,*.cpp,*.txt} | tr " " "\n" | wc -l | bc | tr -d "\n"
-echo */*/* | tr ' ' '\n' | grep -E "((\w*)(java|c|cpp|txt)$)" | wc -l | bc | tr -d "\n"
-echo " / 189 problems"
+totals=(9 8 6 12 8 10 9 14 8 11 6 11 8 7 7 26 26)
+chapters=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17)
+
+rm README.md
+echo "Cracking the Coding Interview problems\n" >> README.md
+for (( i=0; i<=16; i++  ))
+do
+  x=`echo */*/* | tr " " "\n" | grep  -E "((ch${chapters[$i]}.*)(java|c|cpp|txt)$)" | wc -l | bc`
+  echo "ch${chapters[$i]} - ${x} / ${totals[$i]}" >> README.md
+done
+
+echo "\nTotal" >> README.md
+echo */*/* | tr ' ' '\n' | grep -E "((\w*)(java|c|cpp|txt)$)" | wc -l | bc | tr -d "\n" >> README.md
+echo " / 189 problems\n" >> README.md
