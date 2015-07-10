46307fefab542af0a8e10834a6685b8e3cf7638f
schwar12 <schwar12@purdue.edu>
2015-07-09 21:07:42 -0500

    count script

--- count.sh
+++ count.sh
@@ -1,2 +1,3 @@
-echo */*/*.java | tr " " "\n" | wc -l | bc | tr -d "\n"
+#echo */*/{*.java,*.c,*.cpp,*.txt} | tr " " "\n" | wc -l | bc | tr -d "\n"
+echo */*/* | tr ' ' '\n' | grep -E "((\w*)(java|c|cpp|txt)$)" | wc -l | bc | tr -d "\n"
 echo " / 189 problems"
