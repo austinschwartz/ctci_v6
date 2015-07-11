ffc2f3770c6a40775f28260ba2636b99aa8f7a27
schwar12 <schwar12@purdue.edu>
2015-07-10 19:23:54 -0500

    fixed count

--- count.sh
+++ count.sh
@@ -1,14 +1,16 @@
 totals=(9 8 6 12 8 10 9 14 8 11 6 11 8 7 7 26 26)
 chapters=(01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17)
-
-rm README.md
-echo "Cracking the Coding Interview problems\n" >> README.md
+name=README.txt
+rm ${name}
+echo "Cracking the Coding Interview problems\n" >> ${name}
 for (( i=0; i<=16; i++  ))
 do
   x=`echo */*/* | tr " " "\n" | grep  -E "((ch${chapters[$i]}.*)(java|c|cpp|txt)$)" | wc -l | bc`
-  echo "ch${chapters[$i]} - ${x} / ${totals[$i]}" >> README.md
+  echo "ch${chapters[$i]} - ${x} / ${totals[$i]}" >> ${name}
 done
 
-echo "\nTotal" >> README.md
-echo */*/* | tr ' ' '\n' | grep -E "((\w*)(java|c|cpp|txt)$)" | wc -l | bc | tr -d "\n" >> README.md
-echo " / 189 problems\n" >> README.md
+echo "\nTotal" >> ${name}
+echo */*/* | tr ' ' '\n' | grep -E "((\w*)(java|c|cpp|txt)$)" | wc -l | bc | tr -d "\n" >> ${name}
+echo " / 189 problems\n" >> ${name}
+
+cat ${name}
