56f8f534180793ffa34a0e3be5c39725420a1d0a
nawns <nonis@protonmail.com>
2015-10-04 20:08:38 -0400

    Fixing issue #1

--- ch01/1.09_-_String_Rotation/StringRotation.java
+++ ch01/1.09_-_String_Rotation/StringRotation.java
@@ -2,11 +2,15 @@ import java.util.*;
 
 public class StringRotation {
   public static boolean isRotation(String s1, String s2) {
+    if (s1.length() != s2.length()) 
+      return false;
+
     String s3 = s1 + s1;
     return s3.contains(s2);
   }
   public static void main(String[] args) {
     System.out.println(isRotation("waterbottle", "erbottlewat") ? "True" : "False");
     System.out.println(isRotation("waterbottl", "erbottlewat") ? "True" : "False");
+    System.out.println(isRotation("pandeesh", "deeshpand") ? "True" : "False");
   }
 }
