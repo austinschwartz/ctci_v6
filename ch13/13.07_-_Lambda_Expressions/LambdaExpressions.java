e3dde1e9f224f1e72c8b19d036748c618fef6e7b
nawns <nonis@protonmail.com>
2015-11-04 00:01:42 -0500

    fixed 13.07

--- ch13/13.07_-_Lambda_Expressions/LambdaExpressions.java
+++ ch13/13.07_-_Lambda_Expressions/LambdaExpressions.java
@@ -1,6 +1,37 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Country {
+  String continent;
+  int population;
+  String getContinent() {
+    return continent;
+  }
+  int getPopulation() {
+    return population;
+  }
+}
 public class LambdaExpressions {
-  int getPopulation(List<Country> countries, String continent) {
-    Stream<Integer> populations = countries.stream().map(c -> c.getContinent().equals(continent) ? c.getPopulation() : 0);
-    return populations.reduce(0, (a, b) -> a + b);
+  public static int getPopulation(List<Country> countries, String continent) {
+    Stream<Country> sublist = countries
+                            .stream()
+                            .filter(country -> { return country.getContinent().equals(continent);});
+
+    Stream<Integer> populations = sublist.map(c -> c.getPopulation());
+
+    int population = populations.reduce(0, (a, b) -> a + b);
+
+    return population;
+  }
+  public static void main(String[] args) {
+    Country a = new Country(); a.population = 55; a.continent = "A";
+    Country b = new Country(); b.population = 125; b.continent = "B";
+    Country c = new Country(); c.population = 2; c.continent = "A";
+    List<Country> list = new ArrayList<Country>(){{
+      add(a);
+      add(b);
+      add(c);
+    }};
+    System.out.println(getPopulation(list, "A"));
   }
 }
